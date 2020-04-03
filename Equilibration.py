f4780aa544396884f0c000408b490dca8c910a16
Oliver Cohen <ollie.oliver@live.co.uk>
2020-04-03 11:27:18 +0100

    cleaned up code and made equilibration run

---
+++ Equilibration.py
@@ -0,0 +1,64 @@
+import ray
+from QuantumLib import *
+from sys import argv
+from os import environ
+from time import time
+
+
+
+n = int(argv[1])
+processors = int(argv[0])
+epsilon = 0.1
+
+environ["OMP_NUM_THREADS"] = str(processors) # export OMP_NUM_THREADS=4
+environ["OPENBLAS_NUM_THREADS"] = str(processors) # export OPENBLAS_NUM_THREADS=4 
+environ["MKL_NUM_THREADS"] = str(processors) # export MKL_NUM_THREADS=6
+environ["VECLIB_MAXIMUM_THREADS"] =str(processors) # export VECLIB_MAXIMUM_THREADS=4
+environ["NUMEXPR_NUM_THREADS"] = str(processors) # export NUMEXPR_NUM_THREADS=6
+
+
+ray.init()
+
+start = time()
+
+
+state1 = tensor([basis(2,0)]*n)
+
+beta1 = lambda n, m: 1
+beta0 = lambda n, m: 0
+
+alpha1 = Heisenberg1dRingGen(-1,1,1,n)
+
+H1 = hamiltonian(alpha1,beta0, n)
+H1=H1/H1.norm()
+
+H2 = hamiltonian(alpha1,beta1,n)
+H2 = H2/H2.norm()
+
+H3 = hamiltonian(alpha1,beta0,n)
+H3 = H3/H3.norm()
+
+Pertubation = random_herm_oper(H3.dims)
+Pertubation = Pertubation/Pertubation.norm()
+
+H3 = H3 + Pertubation
+H3 = H3/H3.norm()
+
+
+assert(H1.isherm)
+assert(H2.isherm)
+assert(H3.isherm)
+
+energys1, states1 = H1.eigenstates()
+energys2, states2 = H2.eigenstates()
+energys3, states3 = H3.eigenstates()
+
+
+
+equilibration_analyser_p(energys1, states1, state1, 0, 1e5, 200, "H1", _proc=processors)
+equilibration_analyser_p(energys2, states2, state1, 0, 1e5, 200, "H2", _proc=processors)
+equilibration_analyser_p(energys3, states3, state1, 0, 1e5, 200, "H3", _proc=processors)
+
+end = time()
+
+print(f"Completed taking {end-start} seconds")
---
+++ QuantumLib.py
@@ -0,0 +1,230 @@
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+from numpy import linspace, exp, sqrt
+import ray
+from qutip import Qobj, sigmax, sigmay, sigmaz, identity, tensor
+from qutip.metrics import tracedist
+from qutip.states import basis
+from qutip.random_objects import rand_unitary_haar
+from scipy.sparse import csr_matrix
+
+from itertools import chain, product
+
+sigma = [sigmax(),sigmay(),sigmaz()]
+
+def Heisenberg1dRingGen(Jx, Jy, Jz, N):
+    
+    def return_func(n,m,i,j):
+        if (i==j):
+            if(abs(n-m)==1):
+                return -1/2*[Jx,Jy,Jz][i]
+            if((n==(N-1) and m==0) or (n==0 and m==(N-1))):
+                return -1/2*[Jx, Jy, Jz][i]
+        return 0
+    
+    return return_func
+
+
+def Heisenberg1dChainGen(Jx, Jy, Jz, N):
+
+    def return_func(n,m,i,j):
+        if (i==j):
+            if(abs(n-m)==1):
+                return -1/2*[Jx, Jy, Jz][i]
+        return 0
+    
+    return return_func
+
+
+def hamiltonian_spin_interaction_component(alpha, n, m, i, j, N):
+    """
+    Generates a component of the hamiltonian due to a two spins interacting. 
+    
+    Params: n,m two particles which the interaction is between.
+           i,j two particles spin components which interaction is between (0|x, 1|y, 2|z)
+           N   number of particles in the system
+    
+    Returns: Tensor product of the N matrices (quantum obj)
+    """
+
+    if n == m:
+        raise ValueError("Cannot have spin interaction with itself n==m")
+    
+    big_index = max(n, m)  #find out which of n and m comes first in the tensor product 
+    small_index = min(n, m)
+
+    #To construct: split product into three sections  I⊗ I⊗ ....⊗ |   σ_i   |  ⊗ ..... ⊗ | σ_j  | ⊗ I ⊗ I .....I 
+    #                                                 <------1st---->          <-----2nd----->      <--------3rd ---->   
+    
+    fst_section = [identity(2)]*(small_index) 
+    sec_section = [identity(2)]*(big_index-small_index-1)
+    thrd_section = [identity(2)]*(N-big_index-1)
+    operators = fst_section+[sigma[i]]+sec_section+[sigma[j]]+thrd_section
+
+    return alpha*tensor(operators)
+
+
+def hamiltonian_spin_on_site_component(beta, n, i, N):
+    """
+    Generates a component of the hamiltonian due to a spin interacting with an enviromental bias. 
+    
+    Params: n particle which is interacting.
+           i particle spin component which is interacting (0|x, 1|y, 2|z)
+           N   number of particles in the system
+    
+    Returns: Tensor product of the N matrices (quantum obj)
+    """
+    fst = [identity(2)]*n    
+    sec = [identity(2)]*(N-n-1)
+
+    return beta*tensor(fst+[sigma[i]]+sec)
+
+
+def hamiltonian(alpha, beta, N):
+    
+    """
+    Creates the hamiltonian for a given alpha function that specifies the structure of the system.
+
+    Params: alpha - specifies the structure of the system and how a given particle interacts with others and the enviroment
+            beta - specifes how each individual atom interacts with an external field/interaction
+            N - number of particles 
+
+    Returns: Hamiltonian Matrix
+    """
+
+
+    spin_components = (hamiltonian_spin_interaction_component(alpha(n,m,i,j),n,m,i,j,N) for n,m,i,j in product(range(N), range(N), range(3), range(3)) if n != m)
+    interaction_components = (hamiltonian_spin_on_site_component(beta(n,i),n,i,N) for n,i in product(range(N), range(3)))
+            
+    return sum(interaction_components)+sum(spin_components)
+    
+
+def basis_vectors(n):
+    """
+    Generator that yields the basis vectors
+    with correct tensor dimensional form 
+    """
+
+    single_vectors = [basis(2,0) ,basis(2,1)]
+    for selection in product(*([single_vectors]*n)):
+        yield tensor(*selection)
+
+
+def get_thermal_state(states):
+    prob = 1/len(states)
+    return prob*sum(state*state.dag() for state in states)
+
+
+def eff_dim(dens_oper):
+    """
+    Returns effective dimension of mixed state:
+    Via formula 1/Tr(rho^2)
+
+    """
+    dens_oper_sq = dens_oper**2
+    return 1/(dens_oper_sq.tr())
+
+def make_hermitian(h):
+    return 0.5*(h + h.dag())
+
+
+def random_herm_oper(_dims):
+    
+    return make_hermitian(rand_unitary_haar(2**n, dims =_dims))
+
+
+def ket2dmR(state):
+    
+    return csr_matrix(state.data)@csr_matrix(state.data).transpose().conjugate()
+
+
+@ray.remote
+def eq_terms(states, coefs, start, end):
+    
+    return sum(abs(coefs[i]**2)*ket2dmR(states[i]) for i in range(start, end))
+
+
+def get_equilibrated_dens_op_P(states, coefs, n , proc=4):
+
+    number = len(states)//proc
+  
+    s_id = ray.put(states)
+    c_id = ray.put(coefs)
+
+    results = [eq_terms.remote(s_id, c_id, i*number, (i+1)*number) for i in range(proc-1)]
+    results.append(eq_terms.remote(s_id, c_id, (proc-1)*number, len(states)))
+
+    results_val = ray.get(results)
+    
+    return Qobj(sum(results_val), dims=[[2]*n, [2]*n])
+    
+
+def phase(t, E):
+   
+    return exp(-1j*E*t)
+
+@ray.remote
+def time_step(coef, eigenstates, eigenenergies, func, times, strt, stop):
+    
+    result = []
+    
+    for time in times[strt:stop]:
+        state = sum(phase(energy, time)*coef[ind]*eigenstates[ind] for ind, energy in enumerate(eigenenergies))
+        result.append(func(state))
+    
+    return result
+
+
+def simulate(energys, states, coef, t_start, t_end, steps, ret_func=lambda x: x, proc=4):
+    
+    times = linspace(t_start, t_end, steps)
+
+    energys_id = ray.put(energys)
+    states_id = ray.put(states)
+    coef_id = ray.put(coef)
+    times_id = ray.put(times)
+
+    num = steps//proc
+
+    result = [time_step.remote(coef_id, states_id, energys_id, ret_func, times_id, num*i, num*(i+1)) for i in range(proc-1)]
+    result.append(time_step.remote(coef_id, states_id, energys_id, ret_func, times_id, (proc-1)*num, steps))
+    
+    results = ray.get(result)
+    
+    return list(chain(*results))
+    
+    
+def equilibration_analyser_p(energys, states, init_state, start, stop, steps, name, trace=[0], _proc=4):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+   
+    coef = [init_state.overlap(state) for state in states]
+                               
+    equilibrated_dens_op = get_equilibrated_dens_op_P(energys, states, coef, init_state)
+    effective_dimension = eff_dim(equilibrated_dens_op)
+    
+    #now we have the actual effective dimension trace over as can't do it before or messes up
+    
+    equilibrated_dens_op = equilibrated_dens_op.ptrace(trace)
+    
+    bound = 0.5*sqrt(2**len(trace)**2/effective_dimension)
+          
+    trace_dist_compare = lambda state: tracedist(equilibrated_dens_op, state.ptrace(trace))
+    
+    trace_distances = simulate(energys, states, coef, init_state, start, stop, steps, ret_func=trace_dist_compare, proc=_proc)
+    
+    times = [start+step*(stop-start)/steps for step in range(steps)]
+    
+    bound_line = [bound]*steps
+    
+    ax.plot(times, trace_distances, label="Trace-Distance")
+    ax.plot(times, bound_line, label="Bound-Distance")
+    plt.title(f"System: effective dimension {effective_dimension:.2f} and bound {bound:.2f} ")
+    ax.set_xlabel(r"Time /$\hbar$s")
+    ax.set_ylabel(r"$TrDist(\rho(t),\omega$)")
+    plt.legend()
+    plt.savefig(name)
+    
